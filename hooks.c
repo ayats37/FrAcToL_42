f7c517fa72d6f3fae686f19888b1edf21b4cb2c0
ayats37 <taya@student.1337.ma>
2025-02-20 18:59:04 +0100

    fhdhvgdgv

--- hooks.c
+++ hooks.c
@@ -6,7 +6,7 @@
 /*   By: taya <taya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 11:58:07 by taya              #+#    #+#             */
-/*   Updated: 2025/02/20 17:45:39 by taya             ###   ########.fr       */
+/*   Updated: 2025/02/20 18:50:14 by taya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,20 @@ int key_hook(int keycode, t_data *data)
 }
 int mouse_hook(int button, int x, int y, t_data *data)
 {
+    (void)x;
+    (void)y;
+
     if (button == 4)
     {
-        data->fractal->ofsset.real += (x - data->width / 2) / data->fractal->zoom; 
-        data->fractal->ofsset.imag += (y - data->height / 2) / data->fractal->zoom;
+        // data->fractal->ofsset.real += (x - data->width / 2) / data->fractal->zoom; 
+        // data->fractal->ofsset.imag += (y - data->height / 2) / data->fractal->zoom;
         data->fractal->zoom *= 1.1;
     }
     else if (button == 5)
     {
         data->fractal->zoom /= 1.1;
-        data->fractal->ofsset.real -= (x - data->width / 2) / data->fractal->zoom; 
-        data->fractal->ofsset.imag -= (y - data->height / 2) / data->fractal->zoom;
+        // data->fractal->ofsset.real -= (x - data->width / 2) / data->fractal->zoom; 
+        // data->fractal->ofsset.imag -= (y - data->height / 2) / data->fractal->zoom;
     }
     draw_mandelbrot_fractal(data, data->fractal);
     return (0);
